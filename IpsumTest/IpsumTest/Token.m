d5d5040a05f365feaeccbab7d74da10d1a63b261
Patrick Decoster <p.decoster@dev7.nl>
2012-10-12 15:57:23 +0200

    Removed bogus dir and added Token object

---
+++ IpsumTest/IpsumTest/Token.m
@@ -0,0 +1,15 @@
+//
+//  Token.m
+//  IpsumTest
+//
+//  Created by Patrick Decoster on 09-10-12.
+//  Copyright (c) 2012 Avans Hogeschool. All rights reserved.
+//
+
+#import "Token.h"
+
+@implementation Token
+
+@synthesize key, expire;
+
+@end
