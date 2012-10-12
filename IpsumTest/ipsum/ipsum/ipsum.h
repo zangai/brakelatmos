2764d43954a045f9470674310c19a2e4e2f9f8f5
Patrick Decoster <p.decoster@dev7.nl>
2012-10-12 15:52:52 +0200

    Attempting to fix this mess

---
+++ IpsumTest/ipsum/ipsum/ipsum.h
@@ -0,0 +1,17 @@
+//
+//  ipsum.h
+//  ipsum
+//
+//  Created by Patrick Decoster on 09-10-12.
+//  Copyright (c) 2012 Avans Hogeschool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ipsum : NSObject
+
+-(NSString *) hello {
+    return @"Hello World!";
+}
+
+@end
