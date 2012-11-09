82bbf0d9f9dfe10e365f07b6d20a854fffbb07c4
CHRISTIAAN Rakowski <csrakows@imac-005.lab.aim.avans.nl>
2012-11-09 10:36:57 +0100

    Dynamic UI Manager aangemaakt

---
+++ Main/webservice test/DynamicUIManger.h
@@ -0,0 +1,13 @@
+//
+//  DynamicUIManger.h
+//  webservice test
+//
+//  Created by Christiaan Rakowski on 11/9/12.
+//  Copyright (c) 2012 Christiaan Rakowski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DynamicUIManger : NSObject
+
+@end
---
+++ Main/webservice test/DynamicUIManger.m
@@ -0,0 +1,13 @@
+//
+//  DynamicUIManger.m
+//  webservice test
+//
+//  Created by Christiaan Rakowski on 11/9/12.
+//  Copyright (c) 2012 Christiaan Rakowski. All rights reserved.
+//
+
+#import "DynamicUIManger.h"
+
+@implementation DynamicUIManger
+
+@end
