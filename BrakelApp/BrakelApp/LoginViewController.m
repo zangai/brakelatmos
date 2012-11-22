125a09be69b1aea7c0b473d62f3217f063620233
Christiaan Rakowski <cs.rakowski@student.avans.nl>
2012-11-22 14:47:49 +0100

    Test device toegevoegd

--- BrakelApp/BrakelApp/LoginViewController.m
+++ BrakelApp/BrakelApp/LoginViewController.m
@@ -76,6 +76,7 @@
             
             formData = [formData stringByAppendingString:hash];
             //[self makeApiCall:@"login" formdata:formData];
+            formData = [formData stringByAppendingString:@"&device=643b234ee3d38170b4282fee7d9ec54a505f2605e03699f114791e0ca6f3ffb1"];
             
             APILibrary* lib = [[APILibrary alloc] init];
             [lib makeApiCall:@"login" formdata:formData delegate:self handleBy:@selector(callHandler:response:)];
