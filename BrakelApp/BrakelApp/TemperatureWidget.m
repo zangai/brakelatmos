c459cf31ee6aaf9910c006b30fc4ca5fd4fcd415
Tjitte de Visscher <tjittedevisscher@pc120739.lab.aim.avans.nl>
2012-11-23 13:08:01 +0100

    Big improvements on TempWidget, Graph grid is now being drawn with help of Core Plot!

--- BrakelApp/BrakelApp/TemperatureWidget.m
+++ BrakelApp/BrakelApp/TemperatureWidget.m
@@ -14,6 +14,54 @@
 
 @synthesize button;
 @synthesize context;
+@synthesize hostView = hostView_;
+
+
+
+#pragma mark - Chart behavior
+-(void)initPlot {
+    [self configureHost];
+    [self configureGraph];
+    [self configurePlots];
+    [self configureAxes];
+}
+
+-(void)configureHost {
+
+    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:self.bounds];
+    self.hostView.allowPinchScaling = YES;
+    [self addSubview:self.hostView];
+}
+
+-(void)configureGraph {
+    // 1 - Create the graph
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    [graph applyTheme:[CPTTheme themeNamed:kCPTDarkGradientTheme]];
+    self.hostView.hostedGraph = graph;
+    // 2 - Set graph title
+    NSString *title = @"Showing Temperature";
+    graph.title = title;
+    // 3 - Create and set text style
+    CPTMutableTextStyle *titleStyle = [CPTMutableTextStyle textStyle];
+    titleStyle.color = [CPTColor whiteColor];
+    titleStyle.fontName = @"Helvetica-Bold";
+    titleStyle.fontSize = 16.0f;
+    graph.titleTextStyle = titleStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, 10.0f);
+    // 4 - Set padding for plot area
+    [graph.plotAreaFrame setPaddingLeft:30.0f];
+    [graph.plotAreaFrame setPaddingBottom:30.0f];
+    // 5 - Enable user interactions for plot space
+    CPTXYPlotSpace *plotSpace = (CPTXYPlotSpace *) graph.defaultPlotSpace;
+    plotSpace.allowsUserInteraction = YES;
+}
+
+-(void)configurePlots {
+}
+
+-(void)configureAxes {
+}
 
 -(id)initWithJson:(NSDictionary*)json
 {
@@ -29,20 +77,10 @@
 - (void)drawRect:(CGRect)rect
 {
     NSLog(@"temperature widget is showing now");
-    button = [UIButton buttonWithType:UIButtonTypeInfoLight];
-    button.frame = CGRectMake((float)self.bounds.origin.x, (float)self.bounds.origin.y+(float)self.bounds.size.height, (float)self.bounds.size.width, 50.0);
-    [button setTitle:@"Showing temperature" forState:UIControlStateNormal];
-    CGColorRef redColor =
-    [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0].CGColor;
-    context = UIGraphicsGetCurrentContext();
-    CGContextSetStrokeColorWithColor(context, redColor);
-    CGContextSetFillColorWithColor(context,redColor);
-    CGContextFillRect(context, self.bounds);
-    CGContextSetLineWidth(context, 5.0);
-    CGContextAddRect(context, self.bounds);
-    CGContextStrokePath(context);
-    CGContextClosePath(context);
-    CGContextDrawPath(context, kCGPathFillStroke);
+    
+    NSLog([NSString stringWithFormat:@"Long %f", self.bounds.size.width]);
+    [self initPlot];
+
     [self drawGrid:rect];
     [self addSubview:button];
 }
@@ -65,8 +103,14 @@
         CGContextStrokePath(context);
         CGContextStrokePath(context);
     }
-    
-    
-    
+}
+
+#pragma mark - CPTPlotDataSource methods
+-(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot {
+    return 0;
+}
+
+-(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index {
+    return [NSDecimalNumber zero];
 }
 @end
