9637385903c5908767acf3981b1fda88b58a0771
wlmligte <wlmligte@imac-007.lab.aim.avans.nl>
2012-12-07 15:19:48 +0100

    windiwdget erin

--- BrakelApp/BrakelApp/WindWidget.h
+++ BrakelApp/BrakelApp/WindWidget.h
@@ -2,14 +2,65 @@
 //  WindWidget.h
 //  BrakelApp
 //
-//  Created by wlmligte on 12/6/12.
+//  Created by Joost on 06-12-12.
 //  Copyright (c) 2012 Brakel Atmos. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 #import "Widget.h"
+#import <QuartzCore/QuartzCore.h>
+#define graphTitle             "Wind level"
+#define yAxisName              "Wind percentage"
+#define xAxisName               "Time"
+#define gemmideldeWindlabel  "Average:"
+#define huidigWindlabel      "Current:"
+#define paddingXleftLabels  25
 
-@interface WindWidget :Widget
+
+@interface WindWidget : Widget <CPTPlotDataSource>
+{
+    //Buttons
+    UIButton *showWeek;
+    UIButton *showMaand;
+    UIButton *showJaar;
+    UIButton *showDag;
+    
+    //Views
+    UIView   *bottomView;
+    UIView   *leftView;
+    CPTGraphHostingView *graphView;
+    UIImageView *logo;
+    
+    //Labels
+    UILabel *gemmWind;
+    UILabel *huidWind;
+    UILabel *gemmWindValue;
+    UILabel *huidigWindValue;
+    UIColor *iphoneBlue;
+    
+    NSMutableArray *xAxisValues;
+    
+}
+
+
+//Views
+@property (nonatomic, strong) CPTGraphHostingView *graphView;
+@property (nonatomic, strong) UIView *bottomView;
+@property (nonatomic, strong) UIView *leftView;
+@property (nonatomic, strong) UIView *midBorder;
+@property (nonatomic, strong) UIImageView *logo;
+
+//Buttons
+@property (nonatomic, strong) UIButton *showWeek;
+@property (nonatomic, strong) UIButton *showMaand;
+@property (nonatomic, strong) UIButton *showJaar;
+@property (nonatomic, strong) UIButton *showDag;
+
+//Labels
+@property (nonatomic, strong) UILabel *gemmWind;
+@property (nonatomic, strong) UILabel *huidWind;
+@property (nonatomic, strong) UILabel *gemmWindValue;
+@property (nonatomic, strong) UILabel *huidigWindValue;
 
 +(WindWidget*)makeWidgetWithType:(NSString*)type jsonData:(NSDictionary*)json;
+
 @end
