770579c2e8ed76045628e345fe4b9d8ef956c4b6
Tjitte de Visscher <tjittedevisscher@pc120739.lab.aim.avans.nl>
2012-12-07 15:14:21 +0100

    integrated Home button  :| :\ omg

--- BrakelApp/BrakelApp/containerViewController.m
+++ BrakelApp/BrakelApp/containerViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "containerViewController.h"
-#import "dataStorage.h"
+
 
 @interface containerViewController ()
 
@@ -16,6 +16,8 @@
 @implementation containerViewController
 
 @synthesize container;
+@synthesize headerImage;
+@synthesize gebouwNaam;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,16 +31,43 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    UIColor  *iphoneBlue =
+    [UIColor colorWithRed:0.121653f green:0.558395f blue:0.837748f alpha:0];
     UIButton* button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    int w = 100;
-    int h = 30;
-    int x = (self.view.frame.size.width - w);
-    int y = 50;
+    int w = self.view.frame.size.width/6;
+    int h = self.view.frame.size.height/24;
+    int x = widthHeaderImage-(w);
+    int y = heightHeaderImage-(h);
     CGRect rect = { {x, y}, {w, h} };
     [button setFrame:rect];
-    [button setTitle:[[dataStorage sharedManager] buildingTitle] forState:UIControlStateNormal];
-    [self.view addSubview:button];
+    gebouwNaam = [[UILabel alloc]initWithFrame:CGRectMake(x-(2*w), y, w, h)];
+    [gebouwNaam setAdjustsFontSizeToFitWidth:YES];
+    [gebouwNaam setText:[[dataStorage sharedManager] buildingTitle]];
+    [gebouwNaam setBackgroundColor:iphoneBlue];
+    [gebouwNaam setTextColor:[UIColor whiteColor]];
+    [gebouwNaam setText:@"Welkom"];
+    NSLog(@"Dit is de buildingTitle: %@", [[dataStorage sharedManager] buildingTitle] );
+    if([[[dataStorage sharedManager] buildingTitle] isEqualToString:@""])
+    {
+    }
+    else
+    {
+        [gebouwNaam setText:[[dataStorage sharedManager] buildingTitle]];
+        //[self.view addSubview:button];
+    }
+    [button setTitle:@"Home" forState:UIControlStateNormal];
+    [self.view addSubview:gebouwNaam];
+    //[button addTarget:self
+    //            action:@selector(goToHome)
+    //  forControlEvents:UIControlEventTouchUpInside];
+}
+-(void)goToHome
+{
+        CarouselViewController *myNewVC = [[CarouselViewController alloc] init];
+        
+        // do any setup you need for myNewVC
+        
+        [self presentModalViewController:myNewVC animated:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,4 +76,6 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)Home:(id)sender {
+}
 @end
