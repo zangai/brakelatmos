4dab27df6a62c46fa8f78bb74a003f2c223d35f5
csrakowski <cs.rakowski@gmail.com>
2012-12-14 15:23:20 +0100

    iets verder al, denk ik

--- IpsumTest/IpsumTest/Ipsum.m
+++ IpsumTest/IpsumTest/Ipsum.m
@@ -99,10 +99,32 @@
 
 -(void)getLocations:(WebRequestCallback)completion {
     self.callback = completion;
-    NSString * hash = [self sha1:[NSString stringWithFormat:@"/select/%@/%@/@%", token.key, @"2033", _privateKey]];
-    NSString * uri = [NSString stringWithFormat:@"%@/select/%@/%@/%@", _host, token.key, @"2033", hash];
     
-    NSLog(@"Ipsum Request URI: %@", uri);
+    int userId = 2033;
+    
+    NSString* destination = [NSString stringWithFormat:@"%d", userId];
+    
+    
+    NSDate *date = [[NSDate alloc] init];
+    NSTimeInterval timestamp = [date timeIntervalSince1970];
+    
+    long checksum = timestamp + userId;
+    
+    NSString* checkString = [NSString stringWithFormat:@"%ld", checksum];
+    checkString = [checkString substringFromIndex:[checkString length]];
+    
+    destination = [NSString stringWithFormat:@"%@:%f:%@", destination, timestamp, checkString];
+    destination = [self Base64Encode:[destination dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    
+    NSString* code = [NSString stringWithFormat:@"/select/%@/%@/%@", token.key, destination, _privateKey];
+    NSString * hash = [[self sha1:code] lowercaseString];
+    
+    NSString* request = [NSString stringWithFormat:@"/select/%@/%@/%@", token.key, destination, hash];
+    request = [[request stringByReplacingOccurrencesOfString:@"-" withString:@""] lowercaseString];
+    NSString * uri = [NSString stringWithFormat:@"%@%@/", _host, request];
+    
+    NSLog(@"\nIpsum Request URI: %@\n", uri);
     
     NSString* selectClause = [NSString stringWithFormat:@"<field><name></name></field>"];
     NSString* whereClause = [NSString stringWithFormat:@""];
@@ -142,6 +164,73 @@
         [output appendFormat:@"%02x", digest[i]];
     
     return output;
+}
+
+-(NSString *)Base64Encode:(NSData *)data {
+    //Point to start of the data and set buffer sizes
+    int inLength = [data length];
+    int outLength = ((((inLength * 4)/3)/4)*4) + (((inLength * 4)/3)%4 ? 4 : 0);
+    const char *inputBuffer = [data bytes];
+    char *outputBuffer = malloc(outLength);
+    outputBuffer[outLength] = 0;
+    
+    //64 digit code
+    static char Encode[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    
+    //start the count
+    int cycle = 0;
+    int inpos = 0;
+    int outpos = 0;
+    char temp;
+    
+    //Pad the last to bytes, the outbuffer must always be a multiple of 4
+    outputBuffer[outLength-1] = '=';
+    outputBuffer[outLength-2] = '=';
+    
+    /* http://en.wikipedia.org/wiki/Base64
+     Text content   M           a           n
+     ASCII          77          97          110
+     8 Bit pattern  01001101    01100001    01101110
+     
+     6 Bit pattern  010011  010110  000101  101110
+     Index          19      22      5       46
+     Base64-encoded T       W       F       u
+     */
+    
+    
+    while (inpos < inLength){
+        switch (cycle) {
+            case 0:
+                outputBuffer[outpos++] = Encode[(inputBuffer[inpos]&0xFC)>>2];
+                cycle = 1;
+                break;
+            case 1:
+                temp = (inputBuffer[inpos++]&0x03)<<4;
+                outputBuffer[outpos] = Encode[temp];
+                cycle = 2;
+                break;
+            case 2:
+                outputBuffer[outpos++] = Encode[temp|(inputBuffer[inpos]&0xF0)>> 4];
+                temp = (inputBuffer[inpos++]&0x0F)<<2;
+                outputBuffer[outpos] = Encode[temp];
+                cycle = 3;
+                break;
+            case 3:
+                outputBuffer[outpos++] = Encode[temp|(inputBuffer[inpos]&0xC0)>>6];
+                cycle = 4;
+                break;
+            case 4:
+                outputBuffer[outpos++] = Encode[inputBuffer[inpos++]&0x3f];
+                cycle = 0;
+                break;                          
+            default:
+                cycle = 0;
+                break;
+        }
+    }
+    NSString *pictemp = [NSString stringWithUTF8String:outputBuffer];
+    free(outputBuffer); 
+    return pictemp;
 }
 
 @end
